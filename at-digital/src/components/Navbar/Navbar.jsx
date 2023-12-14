22d0845824e16bca785c8aa48bab7576f9b57528
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2023-12-14 17:49:32 +0530

    update

--- at-digital/src/components/Navbar/Navbar.jsx
+++ at-digital/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
 import "./navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="Navigation">
+      <img src="./img/White-Logo.png" alt="" />
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#news">News</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
